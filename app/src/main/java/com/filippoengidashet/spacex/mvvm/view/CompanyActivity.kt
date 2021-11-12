775fcbf6e89647a7115fbc01d849ee18a9e4f409
Filippo <filippo.eng@gmail.com>
2021-11-12 17:14:34 +0000

    Minor cleanup

--- app/src/main/java/com/filippoengidashet/spacex/mvvm/view/CompanyActivity.kt
+++ app/src/main/java/com/filippoengidashet/spacex/mvvm/view/CompanyActivity.kt
@@ -10,7 +10,6 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.filippoengidashet.spacex.R
 import dagger.hilt.android.AndroidEntryPoint
-import java.util.*
 
 /**
  * @author Filippo 09/11/2021
@@ -25,11 +24,6 @@ class CompanyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Calendar.getInstance(TimeZone.getTimeZone("UTC")).get(Calendar.YEAR)
-        for(i in 1900 until 2022) {
-            println(i)
-        }
-
         toolbar = findViewById<Toolbar>(R.id.toolbar).apply {
             title = ""
         }
@@ -40,11 +34,6 @@ class CompanyActivity : AppCompatActivity() {
         navController = nh.navController
 
         toolbar.setupWithNavController(navController)
-
-//        #3. Check for code smells with klint & detekt
-
-//        #./gradlew ktlintCheck
-//        #./gradlew detekt check
     }
 
     private fun showToast(message: String?, length: Int = Toast.LENGTH_SHORT) {
